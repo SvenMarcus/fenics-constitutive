861f2f9c876393c349eb03ed402453fc847bf795
Sven Marcus <s.marcus@outlook.de>
2025-05-16 14:57:05 +0000

    improved method naming to imply side effects

--- src/fenics_constitutive/solver.py
+++ src/fenics_constitutive/solver.py
@@ -16,26 +16,28 @@ from .maps import SubSpaceMap, build_subspace_map
 from .stress_strain import ufl_mandel_strain
 
 
-class BaseLawProtocol(Protocol):
+class Law(Protocol):
     law: IncrSmallStrainModel
     cells: np.ndarray
     del_grad_u: df.fem.Function
     tangent: df.fem.Function
     history: History | None
 
-    def get_stress_and_tangent(self, solver: Any) -> tuple[np.ndarray, np.ndarray]: ...
+    def update_stress_and_tangent(
+        self, solver: Any
+    ) -> tuple[np.ndarray, np.ndarray]: ...
     def map_to_parent(self, solver: Any) -> None: ...
 
 
 @dataclass
-class SingleLaw(BaseLawProtocol):
+class SingleLaw(Law):
     law: IncrSmallStrainModel
     cells: np.ndarray
     del_grad_u: df.fem.Function
     tangent: df.fem.Function
     history: History | None = None
 
-    def get_stress_and_tangent(self, solver: Any) -> tuple[np.ndarray, np.ndarray]:
+    def update_stress_and_tangent(self, solver: Any) -> tuple[np.ndarray, np.ndarray]:
         solver.stress_1.x.array[:] = solver.stress_0.x.array
         solver.stress_1.x.scatter_forward()
         return solver.stress_1.x.array, solver.tangent.x.array
@@ -46,7 +48,7 @@ class SingleLaw(BaseLawProtocol):
 
 
 @dataclass
-class MultiLaw(BaseLawProtocol):
+class MultiLaw(Law):
     law: IncrSmallStrainModel
     cells: np.ndarray
     del_grad_u: df.fem.Function
@@ -55,7 +57,7 @@ class MultiLaw(BaseLawProtocol):
     submesh_map: SubSpaceMap
     history: History | None = None
 
-    def get_stress_and_tangent(self, solver: Any) -> tuple[np.ndarray, np.ndarray]:
+    def update_stress_and_tangent(self, solver: Any) -> tuple[np.ndarray, np.ndarray]:
         self.submesh_map.map_to_child(solver.stress_0, self.stress)
         return self.stress.x.array, self.tangent.x.array
 
@@ -139,7 +141,7 @@ class IncrSmallStrainProblem(df.fem.petsc.NonlinearProblem):
         QV = df.fem.FunctionSpace(mesh, QVe)
         QT = df.fem.FunctionSpace(mesh, QTe)
 
-        self._laws: list[BaseLawProtocol] = []  # Holds SingleLaw or MultiLaw, type-safe
+        self._laws: list[Law] = []  # Holds SingleLaw or MultiLaw, type-safe
         self._del_t = del_t  # time increment
         self._time = 0  # global time will be updated in the update method
 
@@ -284,17 +286,14 @@ class IncrSmallStrainProblem(df.fem.petsc.NonlinearProblem):
 
         for law in self._laws:
             cells = law.cells
-            # TODO: test this!
-            # Replace with `self._del_grad_u[k].interpolate(...)` in 0.8.0
             self.del_grad_u_expr.eval(
                 cells, law.del_grad_u.x.array.reshape(cells.size, -1)
             )
             law.del_grad_u.x.scatter_forward()
-            stress_input, tangent_input = law.get_stress_and_tangent(self)
+            stress_input, tangent_input = law.update_stress_and_tangent(self)
             history_input = None
             if law.history is not None:
                 history_input = law.history.advance()
-
             with df.common.Timer("constitutive-law-evaluation"):
                 law.law.evaluate(
                     self._time,
@@ -304,9 +303,7 @@ class IncrSmallStrainProblem(df.fem.petsc.NonlinearProblem):
                     tangent_input,
                     history_input,
                 )
-
             law.map_to_parent(self)
-
         self.stress_1.x.scatter_forward()
         self.tangent.x.scatter_forward()
 
